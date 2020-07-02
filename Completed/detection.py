c481e453a9953accd826f3f658fc0804e0ee810a
gaurav <gauravbansal233@gmail.com>
2020-07-02 15:31:57 +0530

    object_detection

--- Completed/detection.py
+++ Completed/detection.py
@@ -4,18 +4,20 @@ import tensorflow as tf
 
 from collections import defaultdict
 
-import label_map_util
+from utils import visualization_utils as vis_util
 
 import cv2
 
-# cap = cv2.VideoCapture(0)
 
 NUM_CLASSES = 90
 
 PATH_TO_LABELS = 'mscoco_label_map.pbtxt'
 
-PATH_TO_CKPT = 'ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03/frozen_inference_graph.pb'
-
+# PATH_TO_CKPT = 'ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03/frozen_inference_graph.pb'
+PATH_TO_CKPT = 'ssd_inception_v2_coco_2018_01_28/frozen_inference_graph.pb'
+objects = ["person", "bench", "cat", "dog", "handbag", "tie", "suitcase", "bottle", "wine glass", "cup",
+        "knife", "spoon", "bowl", "chair", "bed", "dinnig table", "tv", "laptop", "mouse", "remote", "keyboard",
+        "cell phone", "book", "clock", "scissors"]
 class Detector():
     def __init__(self):
         self.detection_graph = tf.Graph()
@@ -26,11 +28,7 @@ class Detector():
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
         
-        label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-        # print(label_map)
-        self.categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-        # print(categories)
-        self.category_index = label_map_util.create_category_index(self.categories)
+        self.category_index = {1: {'id': 1, 'name': 'person'}, 2: {'id': 2, 'name': 'bicycle'}, 3: {'id': 3, 'name': 'car'}, 4: {'id': 4, 'name': 'motorcycle'}, 5: {'id': 5, 'name': 'airplane'}, 6: {'id': 6, 'name': 'bus'}, 7: {'id': 7, 'name': 'train'}, 8: {'id': 8, 'name': 'truck'}, 9: {'id': 9, 'name': 'boat'}, 10: {'id': 10, 'name': 'traffic light'}, 11: {'id': 11, 'name': 'fire hydrant'}, 13: {'id': 13, 'name': 'stop sign'}, 14: {'id': 14, 'name': 'parking meter'}, 15: {'id': 15, 'name': 'bench'}, 16: {'id': 16, 'name': 'bird'}, 17: {'id': 17, 'name': 'cat'}, 18: {'id': 18, 'name': 'dog'}, 19: {'id': 19, 'name': 'horse'}, 20: {'id': 20, 'name': 'sheep'}, 21: {'id': 21, 'name': 'cow'}, 22: {'id': 22, 'name': 'elephant'}, 23: {'id': 23, 'name': 'bear'}, 24: {'id': 24, 'name': 'zebra'}, 25: {'id': 25, 'name': 'giraffe'}, 27: {'id': 27, 'name': 'backpack'}, 28: {'id': 28, 'name': 'umbrella'}, 31: {'id': 31, 'name': 'handbag'}, 32: {'id': 32, 'name': 'tie'}, 33: {'id': 33, 'name': 'suitcase'}, 34: {'id': 34, 'name': 'frisbee'}, 35: {'id': 35, 'name': 'skis'}, 36: {'id': 36, 'name': 'snowboard'}, 37: {'id': 37, 'name': 'sports ball'}, 38: {'id': 38, 'name': 'kite'}, 39: {'id': 39, 'name': 'baseball bat'}, 40: {'id': 40, 'name': 'baseball glove'}, 41: {'id': 41, 'name': 'skateboard'}, 42: {'id': 42, 'name': 'surfboard'}, 43: {'id': 43, 'name': 'tennis racket'}, 44: {'id': 44, 'name': 'bottle'}, 46: {'id': 46, 'name': 'wine glass'}, 47: {'id': 47, 'name': 'cup'}, 48: {'id': 48, 'name': 'fork'}, 49: {'id': 49, 'name': 'knife'}, 50: {'id': 50, 'name': 'spoon'}, 51: {'id': 51, 'name': 'bowl'}, 52: {'id': 52, 'name': 'banana'}, 53: {'id': 53, 'name': 'apple'}, 54: {'id': 54, 'name': 'sandwich'}, 55: {'id': 55, 'name': 'orange'}, 56: {'id': 56, 'name': 'broccoli'}, 57: {'id': 57, 'name': 'carrot'}, 58: {'id': 58, 'name': 'hot dog'}, 59: {'id': 59, 'name': 'pizza'}, 60: {'id': 60, 'name': 'donut'}, 61: {'id': 61, 'name': 'cake'}, 62: {'id': 62, 'name': 'chair'}, 63: {'id': 63, 'name': 'couch'}, 64: {'id': 64, 'name': 'potted plant'}, 65: {'id': 65, 'name': 'bed'}, 67: {'id': 67, 'name': 'dining table'}, 70: {'id': 70, 'name': 'toilet'}, 72: {'id': 72, 'name': 'tv'}, 73: {'id': 73, 'name': 'laptop'}, 74: {'id': 74, 'name': 'mouse'}, 75: {'id': 75, 'name': 'remote'}, 76: {'id': 76, 'name': 'keyboard'}, 77: {'id': 77, 'name': 'cell phone'}, 78: {'id': 78, 'name': 'microwave'}, 79: {'id': 79, 'name': 'oven'}, 80: {'id': 80, 'name': 'toaster'}, 81: {'id': 81, 'name': 'sink'}, 82: {'id': 82, 'name': 'refrigerator'}, 84: {'id': 84, 'name': 'book'}, 85: {'id': 85, 'name': 'clock'}, 86: {'id': 86, 'name': 'vase'}, 87: {'id': 87, 'name': 'scissors'}, 88: {'id': 88, 'name': 'teddy bear'}, 89: {'id': 89, 'name': 'hair drier'}, 90: {'id': 90, 'name': 'toothbrush'}}
         self.session = tf.keras.backend.get_session()
         self.graph = tf.get_default_graph()
         # with self.detection_graph.as_default():
@@ -48,18 +46,21 @@ class Detector():
     def detect_objects(self, image):
         image_np_expanded = np.expand_dims(image, axis=0)
         # Actual detection.
-        (boxes, scores_array, classes, num_detections) = self.sess.run(
+        (boxes, scores, classes, num_detections) = self.sess.run(
             [self.boxes, self.scores, self.classes, self.num_detections],
             feed_dict={self.image_tensor: image_np_expanded})
         string = {}
-        string["objects"] = []
-        string["scores"] = []
         
-        for scores in scores_array:
-            for i, score in enumerate(scores):
-                if(score  != 0):
-                    string["objects"].append(self.category_index[i+1]['name'])
-                    string["scores"].append(float(score))
+        image, names, scores = vis_util.visualize_boxes_and_labels_on_image_array(
+                        image,
+                        np.squeeze(boxes),
+                        np.squeeze(classes).astype(np.int32),
+                        np.squeeze(scores),
+                        self.category_index,
+                        use_normalized_coordinates=True,
+                        line_thickness=8)
+        string['names'] = names
+        string['scores'] = scores
         return string
         
 
