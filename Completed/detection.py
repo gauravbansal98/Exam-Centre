b017c4fd355def076f1bbe42560748aad12bc135
Gaurav Bansal <gauravbansal233@gmail.com>
2020-07-15 17:15:37 +0530

    Update detection.py

--- Completed/detection.py
+++ Completed/detection.py
@@ -45,10 +45,6 @@ class Detector():
 
     def detect_objects(self, image):
         image_np_expanded = np.expand_dims(image, axis=0)
-        # Actual detection.
-        (boxes, scores, classes, num_detections) = self.sess.run(
-            [self.boxes, self.scores, self.classes, self.num_detections],
-            feed_dict={self.image_tensor: image_np_expanded})
         string = {}
         
         image, names, scores = vis_util.visualize_boxes_and_labels_on_image_array(
